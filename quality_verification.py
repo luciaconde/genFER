448c31384d5feba56de02aa476b9079b9c705266
luciaconde <lucia.conde@outlook.com>
2018-10-04 14:57:49 +0200

    Added first draft of module for image quality verification

---
+++ quality_verification.py
@@ -0,0 +1,120 @@
+import os
+import csv
+import shutil
+import numpy as np
+import cv2
+
+eye_corner_left = 39
+eye_corner_right = 42
+nose_side_left = 31
+nose_side_right = 35
+
+def findElementsPositions(csv_header):
+    eye_left = []
+    eye_left.append(csv_header.index(" X_"+str(eye_corner_left))) # X_
+    eye_left.append(csv_header.index(" Y_"+str(eye_corner_left))) # Y_
+    eye_left.append(csv_header.index(" Z_"+str(eye_corner_left))) # Z_
+
+    eye_right = []
+    eye_right.append(csv_header.index(" X_"+str(eye_corner_right))) # X_
+    eye_right.append(csv_header.index(" Y_"+str(eye_corner_right))) # Y_
+    eye_right.append(csv_header.index(" Z_"+str(eye_corner_right))) # Z_
+
+    nose_left = []
+    nose_left.append(csv_header.index(" X_"+str(nose_side_left))) # X_
+    nose_left.append(csv_header.index(" Y_"+str(nose_side_left))) # Y_
+    nose_left.append(csv_header.index(" Z_"+str(nose_side_left))) # Z_
+
+    nose_right = []
+    nose_right.append(csv_header.index(" X_"+str(nose_side_right))) # X_
+    nose_right.append(csv_header.index(" Y_"+str(nose_side_right))) # Y_
+    nose_right.append(csv_header.index(" Z_"+str(nose_side_right))) # Z_
+
+    return eye_left, eye_right, nose_left, nose_right
+
+def calculate3DDistance(p1, p2):
+    sqrt_dst = np.sum(p1**2 + p2**2, axis=0)
+    dst = np.sqrt(sqrt_dst)
+    return dst
+
+def loadCSVData(video_path, video_name):
+    f = open(video_path+video_name+".csv", "rt")
+    reader = csv.reader(f, delimiter=',')
+    confidence = []
+    success = []
+    eye_separation = []
+    nose_separation = []
+
+    csv_header = next(reader)
+    eye_left, eye_right, nose_left, nose_right = findElementsPositions(csv_header)
+
+    for row in reader:
+        confidence.append(float(row[3]))
+        success.append(int(row[4]))
+        # Store 3D positions of relevant points
+        left_eye_pos = [float(row[eye_left[0]]), float(row[eye_left[1]]), float(row[eye_left[2]])]
+        right_eye_pos = [float(row[eye_right[0]]), float(row[eye_right[1]]), float(row[eye_right[2]])]
+
+        nose_left_pos = [float(row[nose_left[0]]), float(row[nose_left[1]]), float(row[nose_left[2]])]
+        nose_right_pos = [float(row[nose_right[0]]), float(row[nose_right[1]]), float(row[nose_right[2]])]
+
+        # Calculate and save eyes and nose distances (in mm)
+        eye_separation.append(calculate3DDistance(np.array(left_eye_pos),np.array(right_eye_pos)))
+        nose_separation.append(calculate3DDistance(np.array(nose_left_pos), np.array(nose_right_pos)))
+
+    return np.array(confidence), np.array(success), np.array(eye_separation), np.array(nose_separation)
+
+def checkConfidenceLevels(num_frames, confidence, threshold_confid, threshold_frames):
+    discardVideo = False
+    bad_frames = 0
+    for element in confidence:
+        if element < threshold_confid:
+            bad_frames += 1
+    if float(bad_frames)/float(num_frames) >= threshold_frames:
+        discardVideo = True
+    return discardVideo
+    
+def checkSuccessLevels(num_frames, success, threshold):
+    discardVideo = False
+    bad_frames = 0
+    for element in success:
+        if element != 1:
+            bad_frames += 1
+    if float(bad_frames)/float(num_frames) >= threshold:
+        discardVideo = True
+    return discardVideo
+
+def checkShapeDeformation(eyes_sep, nose_sep):
+    discardVideo = False
+    if np.std(eyes_sep) >= 100.0 or np.std(nose_sep) >= 100.0:
+        discardVideo = True
+    return discardVideo
+
+
+def verifyQuality(video_path, video_name):
+    # Main function where to include calls to rest of image quality verification functions
+    discardVideo = False
+    # Load all relevant data from the CSV file
+    confidence, success, eyes_sep, nose_sep = loadCSVData(video_path, video_name)
+    print 'CONFIDENCE LEVELS:'
+    print confidence
+    print 'NOSE SEPARATION PER FRAME:'
+    print nose_sep
+    num_frames = len(confidence)
+
+    # Start verifying every criterion; if one is not met, directly discard the video
+    discardVideo = checkConfidenceLevels(num_frames, confidence, 0.9, 0.1)
+    if not discardVideo:
+        # Check success levels
+        discardVideo = checkSuccessLevels(num_frames, success, 0.9)
+    
+    if not discardVideo:
+        # Check distances between eyes and nostrils
+        checkShapeDeformation(eyes_sep, nose_sep)
+
+    return discardVideo
+
+
+# TEST 1
+discardVideo = verifyQuality('data/videos/','myrecording4')
+print discardVideo
